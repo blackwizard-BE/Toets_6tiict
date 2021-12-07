af2c7238605f152ac54d63750b92a3683fcbc496
BlackWizard-BE <guillaume.oudouoir@gmail.com>
2021-12-07 09:36:35 +0100

    Add project files.

---
+++ Toets_6tiict/LoginClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEF1
+{
+    internal class LoginClass
+    {
+        private string username;
+        private string password;
+        public string toepassing { get; set; }
+
+        public void Save(string user, string pass, string toep)
+        {
+            username = user;
+            password = pass;
+            toepassing = toep;
+
+        }
+
+        public string OphalenUser()
+        {
+            return username;
+        }
+        public string OphalenPass()
+        {
+            return password;
+        }
+
+    }
+}
---
+++ Toets_6tiict/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OEF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LoginClass[] login = new LoginClass[10];//maakt alle glob variablen aan
+        int account = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnOpslaan_Click(object sender, RoutedEventArgs e)//slaat de toepassing ,login en passwoord op 
+        {
+            login[account] = new LoginClass();
+
+            login[account].Save(txbLogin.Text, txbWachtwoord.Text, txbToepassing.Text);
+            cmbToepassing.Items.Add(txbToepassing.Text);
+            account++;
+
+        }
+
+        private void btnOphalen_Click(object sender, RoutedEventArgs e)//toont alle data in de listbox maar het paswoord is vervangen door ####
+        {
+            lsbRes.Items.Clear();
+            string temp = "";
+            lsbRes.Items.Add(cmbToepassing.Text);
+            lsbRes.Items.Add(login[ToepassingSearch(cmbToepassing.Text)].OphalenUser());
+            for(int i = 0; i< login[ToepassingSearch(cmbToepassing.Text)].OphalenPass().Length; i++)
+            {
+                temp = $"{temp}#";
+            }
+            lsbRes.Items.Add(temp);
+            btnToon.IsEnabled = true;
+        }
+        public int ToepassingSearch(string cmbToep)//zoekt naar de toepasselijke toepassing
+        {
+            bool search = true;
+            int i = 0;
+            do
+            {
+                if (cmbToep == login[i].toepassing)
+                {
+                    search = false;
+                }
+                else
+                {
+                    i++;
+                }
+
+
+
+            }
+            while (search);
+            return i;
+        }
+
+        private void btnToon_Click(object sender, RoutedEventArgs e)//toont het passwoord ipv ####
+        {
+            lsbRes.Items.Clear();
+            lsbRes.Items.Add(cmbToepassing.Text);
+            lsbRes.Items.Add(login[ToepassingSearch(cmbToepassing.Text)].OphalenUser());
+            lsbRes.Items.Add(login[ToepassingSearch(cmbToepassing.Text)].OphalenPass());
+            btnToon.IsEnabled = false;
+
+        }
+    }
+}
